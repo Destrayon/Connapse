2685ed15ec7fd14303e466aaef3b0efe8657ba2f
Destrayon <psummers1050@gmail.com>
2026-09-05 21:28:35 -0500

    fix(azure): add AzureBlob branch to SourceScopePreflight so sources can be created (#477)

--- src/Connapse.Web/Services/SourceScopePreflight.cs
+++ src/Connapse.Web/Services/SourceScopePreflight.cs
@@ -53,6 +53,7 @@ public class SourceScopePreflight(IOptionsMonitor<SourceSecuritySettings> source
         return connection.Provider switch
         {
             ConnectionProvider.S3 => CheckLocation(credential, scope, "bucketName", "bucket", connection.Name),
+            ConnectionProvider.AzureBlob => CheckLocation(credential, scope, "containerName", "container", connection.Name),
             ConnectionProvider.Filesystem => CheckRoot(credential, scope, connection.Name),
             ConnectionProvider.Sftp => CheckSftpScope(credential, scope, connection.Name),
             _ => ScopePreflightResult.Refuse(
